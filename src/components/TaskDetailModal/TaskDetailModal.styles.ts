296ee15ae1098b3db8f42771e8345452d371c289
npels <nicholaspels99@gmail.com>
2025-06-09 17:59:59 -0700

    fix backdrop on modals not going being curved edges

--- src/components/TaskDetailModal/TaskDetailModal.styles.ts
+++ src/components/TaskDetailModal/TaskDetailModal.styles.ts
@@ -11,6 +11,11 @@ export const styles = StyleSheet.create({
 	backdrop: {
 		flex: 1,
 		backgroundColor: "rgba(0, 0, 0, 0.5)",
+		position: "absolute",
+		top: 0,
+		left: 0,
+		right: 0,
+		bottom: 0,
 	},
 	content: {
 		backgroundColor: theme.colors.surface,
